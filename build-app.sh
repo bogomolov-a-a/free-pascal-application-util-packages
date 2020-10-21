68ddd9fc48ddbd7e277131d1a65947492286c890
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-10-21 19:38:42 +0300

    add pesp package to search path

--- build-app.sh
+++ build-app.sh
@@ -1,20 +1,23 @@
-../lazarus/share/lazarus/lazbuild -B --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 freepascal-addition-section-manipulator/src/custapp.addionalsectionmanipulator.lpk
+#prepare 
+git clone git@github.com:bogomolov-a-a/pesp.git
+
+../lazarus/share/lazarus/lazbuild -B -r --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package pesp/src/main/pesp.lpk freepascal-addition-section-manipulator/src/custapp.addionalsectionmanipulator.lpk
 if [ $? -ne 0 ];
 then 
   exit 1;
 fi
-../lazarus/share/lazarus/lazbuild -B --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 freepascal-application-signature-helper/src/custapp.helper.signature.lpk
+../lazarus/share/lazarus/lazbuild -B -r --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 freepascal-application-signature-helper/src/custapp.helper.signature.lpk
 if [ $? -ne 0 ];
 then 
   exit 1;
 fi
-../lazarus/share/lazarus/lazbuild -B --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package freepascal-addition-section-manipulator/src/custapp.addionalsectionmanipulator.lpk freepascal-map-file-based-logger/src/mapbasedfilelogger.lpk
+../lazarus/share/lazarus/lazbuild -B -r --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package pesp/src/main/pesp.lpk --add-package freepascal-addition-section-manipulator/src/custapp.addionalsectionmanipulator.lpk freepascal-map-file-based-logger/src/mapbasedfilelogger.lpk
 if [ $? -ne 0 ];
 then 
   exit 1;
 fi
 #tests
-../lazarus/share/lazarus/lazbuild -B --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package freepascal-addition-section-manipulator/src/custapp.addionalsectionmanipulator.lpk --add-package freepascal-map-file-based-logger/src/mapbasedfilelogger.lpk freepascal-map-file-based-logger/src/logger-section-writer/LogSectionWriter.lpi
+../lazarus/share/lazarus/lazbuild -B -r --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package freepascal-addition-section-manipulator/src/custapp.addionalsectionmanipulator.lpk --add-package freepascal-map-file-based-logger/src/mapbasedfilelogger.lpk --add-package pesp/src/main/pesp.lpk freepascal-map-file-based-logger/src/logger-section-writer/LogSectionWriter.lpi
 if [ $? -ne 0 ];
 then 
   exit 1;
