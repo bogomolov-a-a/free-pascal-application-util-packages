390e05673102724a2c00e07e2cee142eb695fa0c
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-10-21 17:11:56 +0300

    workflow with build lpk added

---
+++ build-app.sh
@@ -0,0 +1,22 @@
+../lazarus/share/lazarus/lazbuild -B --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 freepascal-addition-section-manipulator/src/custapp.addionalsectionmanipulator.lpk
+if [ $? -ne 0 ];
+then 
+  exit 1;
+fi
+../lazarus/share/lazarus/lazbuild -B --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 freepascal-application-signature-helper/src/custapp.helper.signature.lpk
+if [ $? -ne 0 ];
+then 
+  exit 1;
+fi
+../lazarus/share/lazarus/lazbuild -B --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package freepascal-addition-section-manipulator/src/custapp.addionalsectionmanipulator.lpk freepascal-map-file-based-logger/src/mapbasedfilelogger.lpk
+if [ $? -ne 0 ];
+then 
+  exit 1;
+fi
+#tests
+../lazarus/share/lazarus/lazbuild -B --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package freepascal-addition-section-manipulator/src/custapp.addionalsectionmanipulator.lpk --add-package freepascal-map-file-based-logger/src/mapbasedfilelogger.lpk freepascal-map-file-based-logger/src/logger-section-writer/LogSectionWriter.lpi
+if [ $? -ne 0 ];
+then 
+  exit 1;
+fi
+exit 0
