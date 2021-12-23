bd733c39978090c08c08888f01edb228ba2d02b0
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2021-12-23 05:13:03 +0300

    fixed package type

--- build-app.sh
+++ build-app.sh
@@ -18,26 +18,26 @@ then
   exit 1;
 fi
 #build
-../lazarus/share/lazarus/lazbuild -B -r --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package pesp/src/main/pesp.lpk freepascal-addition-section-manipulator/src/main/custapp.addionalsectionmanipulator.lpk
+../lazarus/share/lazarus/lazbuild -B -r --verbose --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package pesp/src/main/pesp.lpk freepascal-addition-section-manipulator/src/main/custapp.addionalsectionmanipulator.lpk
 if [ $? -ne 0 ];
 then 
   clearResoures
   exit 1;
 fi
-../lazarus/share/lazarus/lazbuild -B -r --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 freepascal-application-signature-helper/src/custapp.helper.signature.lpk
+../lazarus/share/lazarus/lazbuild -B -r --verbose --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 freepascal-application-signature-helper/src/custapp.helper.signature.lpk
 if [ $? -ne 0 ];
 then 
   clearResoures
   exit 1;
 fi
-../lazarus/share/lazarus/lazbuild -B -r --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package pesp/src/main/pesp.lpk --add-package freepascal-addition-section-manipulator/src/main/custapp.addionalsectionmanipulator.lpk freepascal-map-file-based-logger/src/mapbasedfilelogger.lpk
+../lazarus/share/lazarus/lazbuild -B -r --verbose --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package pesp/src/main/pesp.lpk --add-package freepascal-addition-section-manipulator/src/main/custapp.addionalsectionmanipulator.lpk freepascal-map-file-based-logger/src/mapbasedfilelogger.lpk
 if [ $? -ne 0 ];
 then 
   clearResoures
   exit 1;
 fi
 #tests
-../lazarus/share/lazarus/lazbuild -B -r --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package freepascal-addition-section-manipulator/src/main/custapp.addionalsectionmanipulator.lpk --add-package freepascal-map-file-based-logger/src/mapbasedfilelogger.lpk --add-package pesp/src/main/pesp.lpk freepascal-map-file-based-logger/src/logger-section-writer/LogSectionWriter.lpi
+../lazarus/share/lazarus/lazbuild -B -r --verbose --lazarusdir=/root/lazarus/share/lazarus --compiler=/root/fpc/ppcx64 --add-package freepascal-addition-section-manipulator/src/main/custapp.addionalsectionmanipulator.lpk --add-package freepascal-map-file-based-logger/src/mapbasedfilelogger.lpk --add-package pesp/src/main/pesp.lpk freepascal-map-file-based-logger/src/logger-section-writer/LogSectionWriter.lpi
 if [ $? -ne 0 ];
 then 
   clearResoures
